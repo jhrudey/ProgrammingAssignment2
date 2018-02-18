966a837faa9585737cec21f972b7c347bb876d6d
jhrudey <the_red_violin@hotmail.com>
2018-02-18 21:19:37 +0100

    Programming Assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## makeCacheMatrix allows the user to enter matrix x into the function and cache the 
+## inverse of that matrix as m. It sets m to NULL in the parent environment
+## before solve is applied so that any 
+## cached, inverted matrices are cleared before caching a new inverted matrix as m.
+## This function also uses the <<- operator 
+## during the setSolve step to ensure that value of m is set in the parent environment
+## and not the child environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+##cacheSolve looks to see if m has been cached and calls up cached data if that is
+## the case. If m has not been cached and is therefore NULL, then cacheSolve carries out 
+## the steps necessary to produce the m, the inverse of matrix x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m
 }
